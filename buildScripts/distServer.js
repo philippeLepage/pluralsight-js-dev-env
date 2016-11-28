790e214cc95918bb26ec9a77ba5eb2e09556ca02
Philippe Lepage <plepage@facilite.com>
2016-11-28 10:26:14 -0500

    deployment implemented

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,15 +15,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req,res) {
-  // hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1, "fileName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2, "fileName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3, "fileName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
