056d306e7823c992514449c41ca180ad3d98790e
Philippe Lepage <plepage@facilite.com>
2016-11-28 10:25:13 -0500

    deployment implemented

---
+++ src/api/baseURL.js
@@ -0,0 +1,13 @@
+export default function getBaseURL() {
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://polar-cove-94979.herokuapp.com/';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
